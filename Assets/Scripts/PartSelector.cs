551a7f079d3da1d50f8e03102c0af48a1f776565
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2026-06-03 15:05:20 +0300

    toka päivä

---
+++ Assets/Scripts/PartSelector.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PartSelector : MonoBehaviour
+{
+    public Sprite[] parts;
+    private SpriteRenderer partRenderer;
+    void Awake()
+    {
+        partRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        partRenderer.sprite = parts[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
